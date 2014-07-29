78742d43e23f1b2e04d76317959871dc40ffc046
Terry Lewis <terrylewis@outlook.com>
2014-07-28 21:24:18 -0500

    Initial Commit

---
+++ SwiftStyledStrings/SwiftStyledStrings.h
@@ -0,0 +1,19 @@
+//
+//  SwiftStyledStrings.h
+//  SwiftStyledStrings
+//
+//  Created by Terry Lewis II on 7/28/14.
+//  Copyright (c) 2014 Blue Plover Productions LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftStyledStrings.
+FOUNDATION_EXPORT double SwiftStyledStringsVersionNumber;
+
+//! Project version string for SwiftStyledStrings.
+FOUNDATION_EXPORT const unsigned char SwiftStyledStringsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftStyledStrings/PublicHeader.h>
+
+
